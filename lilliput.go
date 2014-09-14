6f48ce592b43cc133c06ded18ce82fe568b1a099
Apoorva Moghey <apoorva.moghey@jabong.com>
2014-09-14 19:45:41 +0530

    add redis handler

--- lilliput.go
+++ lilliput.go
@@ -3,21 +3,40 @@ package liliput
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/garyburd/redigo/redis"
 	"github.com/gorilla/mux"
 	"github.com/pilu/base62"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
+	"regexp"
+	"strings"
 )
 
 var Id WebServiceResponse
 
 type WebServiceResponse struct {
-	Id        int
-	MacId     string
-	RegiserId string
+	Id         int
+	MacId      string
+	RegisterId string
 }
+
+type Data struct {
+	Url string
+}
+
+var data Data
+
+type Response struct {
+	Url     string `json:"url"`
+	Err     bool   `json:"err"`
+	Message string `json:"message"`
+}
+
+var response Response
+
+var resource redis.Conn
 
 func init() {
 	interfaces, _ := net.InterfaceByName("eth0")
@@ -38,24 +57,70 @@ func init() {
 	fmt.Println("Registration complete.")
 }
 
+func Db() redis.Conn {
+	if resource == nil {
+		val := fmt.Sprintf("%s:%s",
+			Get("redis.server", ""),
+			Get("redis.port", ""))
+		fmt.Println(val)
+		var err error
+		resource, err = redis.Dial("tcp", val)
+		resource.Do("SELECT", 0)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return resource
+}
+
 func TinyUrl(resp http.ResponseWriter, req *http.Request) {
 	resp.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(req)
-	fmt.Println(params)
-	encoded := base62.Encode(21122)
-	tiny := make(map[string]string)
-	tiny["url"] = Get("lilliput.domain", "").(string) + encoded
-	enc, _ := json.Marshal(tiny)
-	resp.Write(enc)
+	fmt.Println(req.FormValue("url"))
+	data.Url = req.FormValue("url")
+	expression := regexp.MustCompile(`(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?`)
+	fmt.Println(expression.MatchString(data.Url))
+
+	if expression.MatchString(data.Url) {
+		StoreData()
+		response.Err = false
+	} else {
+		response.Err = true
+		response.Message = "Provide valid url,url must be with prepend with http:// or https://"
+	}
+
+	r, _ := json.Marshal(response)
+	resp.Write(r)
 	return
 }
 
+func StoreData() {
+	db := Db()
+	n, err := redis.Int(db.Do("INCR", data.Url))
+	if err != nil {
+		response.Err = true
+		response.Message = "Faild to generate please try again."
+	} else {
+		encoded := base62.Encode(n)
+		response.Url = Get("lilliput.domain", "").(string) + encoded + Id.RegisterId
+		fmt.Println(response.Url)
+	}
+}
+
 func Redirect(resp http.ResponseWriter, req *http.Request) {
-	fmt.Println("Redirecting from " + Id.RegiserId)
+	fmt.Println("Redirecting from " + Id.RegisterId)
 	params := mux.Vars(req)
 	fmt.Println(params["liliput"])
-	//remove last character from params & decode, fectch url from db
-	http.Redirect(resp, req, "http://google.com", 301)
+	encoded := strings.TrimSuffix(params["liliput"], Id.RegisterId)
+	fmt.Println(encoded)
+	decoded := base62.Decode(encoded)
+	fmt.Println(decoded)
+	db := Db()
+	// val, err := redis.String(db.Do("GET", string(decoded)))
+	val, err := db.Do("GET", decoded)
+	fmt.Println(err)
+	fmt.Println(val)
+	//remove last character from params & decode, fetch url from db
+	// http.Redirect(resp, req, "http://google.com", 301)
 }
 
 func Start() {
